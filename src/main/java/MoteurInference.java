1bacc27d9e10f655a519a92d3b4d40046b6a9ace
Caybe <cabezasmathias@gmail.com>
2021-05-11 15:46:40 +0200

    Classe regle

--- src/main/java/MoteurInference.java
+++ src/main/java/MoteurInference.java
@@ -1,2 +1,6 @@
 public class MoteurInference {
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/main/java/Regle.java
+++ src/main/java/Regle.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Regle {
 
@@ -11,19 +12,44 @@ public class Regle {
         this.conclusion = "";
     }
 
-    public void creer() {
+    public void creer(ArrayList<String> premisses, String conclusion) {
         //Saisir des premisses tant que l'utilisateur le demande
-        //Saisir une conclusion
+//        System.out.println("\t Saisir règle : ");
+//        boolean suite = true;
+//        Scanner keyboard = new Scanner(System.in);
+//        while(suite) {
+//            System.out.print("\tSaisir premisse : ");
+//            premisses.add(keyboard.nextLine());
+//            System.out.print("\n\tEncore? (y/n)");
+//            String res = keyboard.nextLine();
+//            while( !res.equals("y") || !res.equals("n")) {
+//                if(res == "y"){
+//                    suite = false;
+//                }
+//
+//                res = keyboard.nextLine();
+//                System.out.print("\nTaper y ou n");
+//            }
+//        }
+//        //Saisir une conclusion
+//        System.out.print("\tSaisir conclusion: ");
+//        conclusion = keyboard.nextLine();
+        this.premisses = premisses;
+        this.conclusion = conclusion;
     }
 
     public void supprimer(String premisse) {
-        // Détruire la règle ou supprimer une premisse?
+        premisses.remove(premisse);
+    }
+
+    public boolean vide() {
+        return premisses.isEmpty();
     }
 
     /**
      * Indique si la premisse passée en paramètre appartient à la règle
      * @param premisse est la premisse dont on vérifie l'existence
-     * @return true si la premisse est présente false sinon
+     * @return true si la premisse est présente, false sinon
      */
     public boolean appartient(String premisse) {
         return premisses.contains(premisse);
